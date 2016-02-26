c2d11d367e2a779797f7ebb83daefcb353aa75e6
Daphne <dg1181@nyu.edu>
2016-02-26 13:07:00 -0800

    fixed rack-cors

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -1,6 +1,6 @@
-require 'rack-cors'
+require 'rack/cors'
 
-config.middleware.insert_before 0, "Rack::Cors" do
+Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
   allow do
     origins '*'
     resource '*', :headers => :any, :methods => [:get, :post, :options]
