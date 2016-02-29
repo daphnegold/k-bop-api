d827c47aa2106b3751b1e72d4ecd455f57201905
Daphne <dg1181@nyu.edu>
2016-02-29 14:14:34 -0800

    Model/controller for playlists, model for songs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225013817) do
+ActiveRecord::Schema.define(version: 20160229221325) do
+
+  create_table "playlists", force: :cascade do |t|
+    t.string   "pid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "uri"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "uid"
