c321298be15560e4a1626dc1b826da6b4a3a15b6
Daphne <dg1181@nyu.edu>
2016-03-10 15:24:00 -0800

    fixed vcr for user controller and model

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe UsersController, :vcr, type: :controller do
   let(:user) { create(:spotify_user) }
   let(:rspotify_object) { RSpotify::User.new(OmniAuth.config.mock_auth[:spotify]) }
 
   describe "#create" do
+    before :each do
+      authentication_time
+    end
     context "auth successful" do
       before { request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:spotify] }
 
       it "redirects to status_path with successful auth" do
-        VCR.use_cassette('api_responses', :record => :new_episodes) do
-          get :create, provider: :spotify
-          expect(response).to redirect_to status_path(user: rspotify_object.id, display: user.display_name.split(" ").first)
-        end
+        get :create, provider: :spotify
+        expect(response).to redirect_to status_path(user: rspotify_object.id, display: user.display_name.split(" ").first)
       end
     end
 
@@ -20,10 +21,8 @@ RSpec.describe UsersController, type: :controller do
       before { request.env["omniauth.auth"] = {} }
 
       it "redirects to status_path with error" do
-        VCR.use_cassette('api_responses', :record => :new_episodes) do
-          get :create, provider: :spotify
-          expect(response).to redirect_to status_path(error: "auth_failure")
-        end
+        get :create, provider: :spotify
+        expect(response).to redirect_to status_path(error: "auth_failure")
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User, :vcr, type: :model do
   let(:user) { build(:spotify_user) }
   let(:rspotify_object) { RSpotify::User.new(OmniAuth.config.mock_auth[:spotify]) }
 
@@ -17,10 +17,9 @@ RSpec.describe User, type: :model do
 
   describe ".find_or_create" do
     it "creates a valid user" do
-      VCR.use_cassette('api_responses', :record => :new_episodes) do
-        spotify_user = User.find_or_create(rspotify_object)
-        expect(spotify_user).to be_valid
-      end
+      authentication_time
+      spotify_user = User.find_or_create(rspotify_object)
+      expect(spotify_user).to be_valid
     end
 
     it "finds a valid user" do
