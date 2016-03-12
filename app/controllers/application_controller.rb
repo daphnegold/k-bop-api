e7da4a2e75a3c9ddd11b5ef4a264e7dfde9845fc
Daphne <dg1181@nyu.edu>
2016-03-11 23:23:50 -0800

    removed json check from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token, if: :json_request?
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 
   def get_comments(track_uri)
     song = Song.find_by(uri: track_uri)
@@ -22,11 +22,5 @@ class ApplicationController < ActionController::Base
     else
       return 0
     end
-  end
-
-  protected
-
-  def json_request?
-    request.format.json?
   end
 end
