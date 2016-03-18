05f98d4ef4e2f1e48d22ab734c75c7d703f8df53
Daphne <dg1181@nyu.edu>
2016-03-18 12:18:17 -0700

    added route for google play store

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,9 @@ class WelcomeController < ApplicationController
   def letsencrypt
     render plain: ENV['LE_AUTH_RESPONSE']
   end
-  
+
+  def index
+    redirect_to 'https://play.google.com/apps/testing/ninja.k.bop'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   get "/.well-known/acme-challenge/#{ENV['LE_AUTH_REQUEST']}", to: 'welcome#letsencrypt'
 
   get "/auth/:provider/callback", to: "users#create"
